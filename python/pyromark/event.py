c5ec1911ca5bd255127f648bd2c1d4338d5d3b4f
monosans <hsyqixco@protonmail.com>
2024-11-01 15:27:42 +0300

    fix: make event.py available in runtime

--- python/pyromark/event.py
+++ python/pyromark/event.py
@@ -1,29 +1,26 @@
-# ruff: noqa: PYI021
-"""This module doesn't exist in runtime, so use it like this:
+from __future__ import annotations
 
-```python
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    import pyromark.event
-```
-"""
+from typing import Optional, Union
 
 from typing_extensions import Literal, TypeAlias, TypedDict
 
 HeadingLevel: TypeAlias = Literal["H1", "H2", "H3", "H4", "H5", "H6"]
 
+
 class Heading(TypedDict):
     level: HeadingLevel
-    id: str | None
+    id: Optional[str]
     classes: tuple[str, ...]
-    attrs: tuple[tuple[str, str | None], ...]
+    attrs: tuple[tuple[str, Optional[str]], ...]
+
 
 BlockQuoteKind: TypeAlias = Literal[
     "Note", "Tip", "Important", "Warning", "Caution"
 ]
 
-CodeBlockKind: TypeAlias = Literal["Indented"] | tuple[Literal["Fenced"], str]
+CodeBlockKind: TypeAlias = Union[
+    Literal["Indented"], tuple[Literal["Fenced"], str]
+]
 
 Alignment: TypeAlias = Literal["None", "Left", "Center", "Right"]
 
@@ -39,11 +36,13 @@ LinkType: TypeAlias = Literal[
     "Email",
 ]
 
+
 class Link(TypedDict):
     link_type: LinkType
     dest_url: str
     title: str
     id: str
+
 
 class Image(TypedDict):
     link_type: LinkType
@@ -51,9 +50,10 @@ class Image(TypedDict):
     title: str
     id: str
 
+
 MetadataBlockKind: TypeAlias = Literal["YamlStyle", "PlusesStyle"]
 
-Tag: TypeAlias = (
+Tag: TypeAlias = Union[
     Literal[
         "Paragraph",
         "HtmlBlock",
@@ -66,19 +66,19 @@ Tag: TypeAlias = (
         "Emphasis",
         "Strong",
         "Strikethrough",
-    ]
-    | tuple[Literal["Heading"], Heading]
-    | tuple[Literal["BlockQuote"], BlockQuoteKind | None]
-    | tuple[Literal["CodeBlock"], CodeBlockKind]
-    | tuple[Literal["List"], int | None]
-    | tuple[Literal["FootnoteDefinition"], str]
-    | tuple[Literal["Table"], tuple[Alignment, ...]]
-    | tuple[Literal["Link"], Link]
-    | tuple[Literal["Image"], Image]
-    | tuple[Literal["MetadataBlock"], MetadataBlockKind]
-)
+    ],
+    tuple[Literal["Heading"], Heading],
+    tuple[Literal["BlockQuote"], Optional[BlockQuoteKind]],
+    tuple[Literal["CodeBlock"], CodeBlockKind],
+    tuple[Literal["List"], Optional[int]],
+    tuple[Literal["FootnoteDefinition"], str],
+    tuple[Literal["Table"], tuple[Alignment, ...]],
+    tuple[Literal["Link"], Link],
+    tuple[Literal["Image"], Image],
+    tuple[Literal["MetadataBlock"], MetadataBlockKind],
+]
 
-TagEnd: TypeAlias = (
+TagEnd: TypeAlias = Union[
     Literal[
         "Paragraph",
         "CodeBlock",
@@ -92,13 +92,13 @@ TagEnd: TypeAlias = (
         "TableHead",
         "TableRow",
         "TableCell",
-    ]
-    | tuple[Heading, HeadingLevel]
-    | tuple[Literal["BlockQuote"], BlockQuoteKind | None]
-    | tuple[Literal["List"], bool]
-)
+    ],
+    tuple[Heading, HeadingLevel],
+    tuple[Literal["BlockQuote"], Optional[BlockQuoteKind]],
+    tuple[Literal["List"], bool],
+]
 
-Event: TypeAlias = (
+Event: TypeAlias = Union[
     Literal[
         "SoftBreak",
         "HardBreak",
@@ -108,16 +108,16 @@ Event: TypeAlias = (
         "Strikethrough",
         "Link",
         "Image",
-    ]
-    | tuple[Literal["Start"], Tag]
-    | tuple[Literal["End"], TagEnd]
-    | tuple[Literal["Text"], str]
-    | tuple[Literal["Code"], str]
-    | tuple[Literal["InlineMath"], str]
-    | tuple[Literal["DisplayMath"], str]
-    | tuple[Literal["Html"], str]
-    | tuple[Literal["InlineHtml"], str]
-    | tuple[Literal["FootnoteReference"], str]
-    | tuple[Literal["TaskListMarker"], bool]
-    | tuple[Literal["MetadataBlock"], MetadataBlockKind]
-)
+    ],
+    tuple[Literal["Start"], Tag],
+    tuple[Literal["End"], TagEnd],
+    tuple[Literal["Text"], str],
+    tuple[Literal["Code"], str],
+    tuple[Literal["InlineMath"], str],
+    tuple[Literal["DisplayMath"], str],
+    tuple[Literal["Html"], str],
+    tuple[Literal["InlineHtml"], str],
+    tuple[Literal["FootnoteReference"], str],
+    tuple[Literal["TaskListMarker"], bool],
+    tuple[Literal["MetadataBlock"], MetadataBlockKind],
+]
