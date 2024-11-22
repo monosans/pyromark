6a241993816f07501e932067edb32601ddd0f151
monosans <hsyqixco@protonmail.com>
2024-11-22 16:04:28 +0300

    fix: make sure files are properly closed in CLI

--- python/pyromark/__main__.py
+++ python/pyromark/__main__.py
@@ -2,13 +2,17 @@
 from __future__ import annotations
 
 import argparse
-from collections.abc import Sequence
+from collections.abc import Iterator, Sequence
+from contextlib import contextmanager
 from typing import Optional
 
 import pyromark
 
 
-def _parse_args(args: Optional[Sequence[str]], /) -> argparse.Namespace:
+@contextmanager
+def _parse_args(
+    args: Optional[Sequence[str]], /
+) -> Iterator[argparse.Namespace]:
     parser = argparse.ArgumentParser(
         pyromark.__name__, description=pyromark.__doc__
     )
@@ -18,7 +22,7 @@ def _parse_args(args: Optional[Sequence[str]], /) -> argparse.Namespace:
     parser.add_argument(
         "-o",
         "--output",
-        type=argparse.FileType("w"),
+        type=argparse.FileType("w", encoding="utf-8"),
         help="output file path, defaults to stdout",
     )
     for opt_name in pyromark.Options.__members__:
@@ -27,30 +31,34 @@ def _parse_args(args: Optional[Sequence[str]], /) -> argparse.Namespace:
         )
     parser.add_argument(
         "file",
-        type=argparse.FileType(),
+        type=argparse.FileType("r", encoding="utf-8"),
         help="input file path or '-' for stdin",
     )
-    return parser.parse_args(args)
+    parsed_args = parser.parse_args(args)
+    try:
+        yield parsed_args
+    finally:
+        parsed_args.file.close()
+        if parsed_args.output is not None:
+            parsed_args.output.close()
 
 
 def _main(args: Optional[Sequence[str]] = None, /) -> None:
-    parsed_args = _parse_args(args)
+    with _parse_args(args) as parsed_args:
+        content = parsed_args.file.read()
+        parsed_args.file.close()
 
-    with parsed_args.file as f:
-        content = f.read()
+        opts = pyromark.Options(0)
+        for opt_name, opt in pyromark.Options.__members__.items():
+            if getattr(parsed_args, opt_name.lower()):
+                opts |= opt
 
-    opts = pyromark.Options(0)
-    for opt_name, opt in pyromark.Options.__members__.items():
-        opt_enabled = getattr(parsed_args, opt_name.lower())
-        if opt_enabled:
-            opts |= opt
-
-    html = pyromark.html(content, options=opts)
-    if parsed_args.output is None:
-        print(html, end="")
-    else:
-        with parsed_args.output:
-            print(html, end="", file=parsed_args.output)
+        html = pyromark.html(content, options=opts)
+        if parsed_args.output is None:
+            print(html, end="")
+        else:
+            parsed_args.output.write(html)
+            parsed_args.output.close()
 
 
 if __name__ == "__main__":
