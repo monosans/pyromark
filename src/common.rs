ab881a94dcd93873ac8fc3faeaec5222aceb1105
monosans <hsyqixco@protonmail.com>
2025-04-24 09:03:26 +0300

    reconfigure clippy

--- src/common.rs
+++ src/common.rs
@@ -1,4 +1,4 @@
-pub(crate) struct PythonizeCustom;
+pub struct PythonizeCustom;
 
 impl<'py> pythonize::PythonizeTypes<'py> for PythonizeCustom {
     type List = pyo3::types::PyTuple;
@@ -7,18 +7,18 @@ impl<'py> pythonize::PythonizeTypes<'py> for PythonizeCustom {
         pythonize::PythonizeUnnamedMappingAdapter<'py, pyo3::types::PyDict>;
 }
 
-pub(crate) fn build_options(options: u32) -> pulldown_cmark::Options {
+pub const fn build_options(options: u32) -> pulldown_cmark::Options {
     pulldown_cmark::Options::from_bits_truncate(options)
 }
 
-pub(crate) fn html(markdown: &str, options: pulldown_cmark::Options) -> String {
+pub fn html(markdown: &str, options: pulldown_cmark::Options) -> String {
     let parser = pulldown_cmark::Parser::new_ext(markdown, options);
     let mut html_output = String::new();
     pulldown_cmark::html::push_html(&mut html_output, parser);
     html_output
 }
 
-pub(crate) fn events(
+pub fn events(
     markdown: &str,
     options: pulldown_cmark::Options,
     merge_text: bool,
@@ -31,7 +31,7 @@ pub(crate) fn events(
     }
 }
 
-pub(crate) fn events_with_range(
+pub fn events_with_range(
     markdown: &str,
     options: pulldown_cmark::Options,
 ) -> Vec<(pulldown_cmark::Event, std::ops::Range<usize>)> {
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,38 @@
+#![warn(
+    clippy::all,
+    clippy::pedantic,
+    clippy::restriction,
+    clippy::nursery,
+    clippy::cargo
+)]
+#![allow(
+    clippy::absolute_paths,
+    clippy::allow_attributes_without_reason,
+    clippy::allow_attributes,
+    clippy::arbitrary_source_item_ordering,
+    clippy::blanket_clippy_restriction_lints,
+    clippy::default_numeric_fallback,
+    clippy::else_if_without_else,
+    clippy::float_arithmetic,
+    clippy::implicit_return,
+    clippy::iter_over_hash_type,
+    clippy::min_ident_chars,
+    clippy::missing_docs_in_private_items,
+    clippy::mod_module_files,
+    clippy::multiple_crate_versions,
+    clippy::pattern_type_mismatch,
+    clippy::pub_with_shorthand,
+    clippy::too_many_lines,
+    clippy::question_mark_used,
+    clippy::shadow_reuse,
+    clippy::shadow_unrelated,
+    clippy::single_call_fn,
+    clippy::single_char_lifetime_names,
+    clippy::std_instead_of_alloc,
+    clippy::std_instead_of_core,
+    clippy::unwrap_used
+)]
+
 mod class_api;
 mod common;
 mod function_api;
